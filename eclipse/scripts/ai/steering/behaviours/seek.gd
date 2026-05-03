161ead162033a8774d0b3d4ff2d633e87313ff74
nv <C22388271@mytudublin.ie>
2026-05-03 18:05:48 +0100

    added seek steering behaviour with gizmo drawing

---
+++ eclipse/scripts/ai/steering/behaviours/seek.gd
@@ -0,0 +1,26 @@
+class_name Seek
+extends SteeringBehaviour
+
+@export var target_path: NodePath
+@export var max_speed := 4.0
+
+var target: Node3D
+
+func _ready():
+	if target_path:
+		target = get_node(target_path)
+
+func calculate() -> Vector3:
+	if target == null:
+		return Vector3.ZERO
+	
+	var to_target = target.global_position - agent.global_position
+	var desired_velocity = to_target.normalized() * max_speed
+	var current_velocity = agent.velocity
+	var force = (desired_velocity - current_velocity) * weight
+	
+	return force
+	
+func on_draw_gizmos():
+	if target:
+		DebugDraw3D.draw_line(agent.global_position, target.global_position, Color.GREEN)
