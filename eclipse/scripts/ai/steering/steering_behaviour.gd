ee54989d500f5362f59017a2a018dc08f46a7c43
nv <C22388271@mytudublin.ie>
2026-05-03 18:33:04 +0100

    Update steering_behaviour.gd

--- eclipse/scripts/ai/steering/steering_behaviour.gd
+++ eclipse/scripts/ai/steering/steering_behaviour.gd
@@ -1,14 +1,26 @@
 class_name SteeringBehaviour
 extends Node
 
-# global weight, controls how strong this behaviour is in final mix
 @export var weight := 1.0
-@export var show_gizmos := true
+@export var drawGizmos := true
+@export var enabled := true : set = set_enabled, get = is_enabled
 
-var agent: CharacterBody3D
+# reference to whatever uses this behaviour
+var agent
+
+func set_enabled(e):
+	enabled = e
+	set_process(enabled)
+
+func is_enabled():
+	return enabled
 
 func calculate() -> Vector3:
 	return Vector3.ZERO
 
 func on_draw_gizmos():
 	pass
+
+func _process(delta):
+	if drawGizmos and enabled:
+		on_draw_gizmos()
