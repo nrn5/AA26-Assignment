28ad7b2e4faf160ba13adf5a3fc238118b35462d
nv <C22388271@mytudublin.ie>
2026-05-03 15:50:18 +0100

    update settings menu

--- eclipse/scripts/ui/settings_menu.gd
+++ eclipse/scripts/ui/settings_menu.gd
@@ -1,11 +1,33 @@
 extends Control
 
+@onready var mouse_sn_val_lbl = $CenterContainer/Panel/mgnSettings/vbxSettingsMenu/vbxMouseSensitivity/lblMouseSnVal
+@onready var music_val_lbl = $CenterContainer/Panel/mgnSettings/vbxSettingsMenu/vbxMusicVol/lblMusicVolVal
+@onready var sfx_val_lbl =$CenterContainer/Panel/mgnSettings/vbxSettingsMenu/vbxSFXVol/lblSFXVolVal
 
-# Called when the node enters the scene tree for the first time.
+signal back_pressed
+var player_ref = null
+
+# { INIT }
 func _ready():
-	pass # Replace with function body.
+	hide()
 
+func set_player(player):
+	player_ref = player
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# { MENU BTNS }
+func _on_back_pressed():
+	emit_signal("back_pressed")
+
+# { MENU SLIDERS } (placeholders for now)
+func _on_mouse_sensitivity_changed(value):
+	if player_ref:
+		player_ref.mouse_sensitivity = value
+	mouse_sn_val_lbl.text = str(int(value)) + "%"
+
+func _on_music_volumn_changed(value):
+	print("Music volume:", value)
+	music_val_lbl.text = str(int(value)) + "%"
+
+func _on_sfx_volume_changed(value):
+	print("SFX volume:", value)
+	sfx_val_lbl.text = str(int(value)) + "%"
