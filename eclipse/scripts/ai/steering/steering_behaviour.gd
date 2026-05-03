d95aafb5f4aec5a45aaea92426ddec08052c9cf2
nv <C22388271@mytudublin.ie>
2026-05-03 18:06:05 +0100

    added parent steering behaviour class

---
+++ eclipse/scripts/ai/steering/steering_behaviour.gd
@@ -0,0 +1,13 @@
+class_name SteeringBehaviour
+extends Node
+
+# global weight, controls how strong this behaviour is in final mix
+@export var weight := 1.0
+
+var agent: CharacterBody3D
+
+func calculate() -> Vector3:
+	return Vector3.ZERO
+
+func on_draw_gizmos():
+	pass
