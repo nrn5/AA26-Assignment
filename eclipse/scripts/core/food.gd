025cd337530aea2f6dd964750e75934fa1f0eaf7
nv <C22388271@mytudublin.ie>
2026-05-05 09:51:46 +0100

    Update food.gd

--- eclipse/scripts/core/food.gd
+++ eclipse/scripts/core/food.gd
@@ -3,13 +3,19 @@ extends Area3D
 
 @export var is_good := true   # good vs bad food
 @export var value := 1
+@export var spin_speed := 1.5
+@export var tilt_angle := 20.0
 
 var active := false
 
 func _ready():
 	await get_tree().process_frame  # wait 1 frame
 	active = true
+	rotation_degrees.x = tilt_angle
 	body_entered.connect(_on_body_entered)
+
+func _process(delta):
+	rotate_y(spin_speed * delta)
 
 func _on_body_entered(body):
 	if not active:
